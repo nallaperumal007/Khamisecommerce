db31eda23563cb3219d32b5ce5c972cfd5d7a925
nallaperumal007 <nallaperumal342@gmail.com>
2025-10-25 21:52:04 +0530

    Categories

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.layout')
+@extends('admin.layout.master')
 
 @section('content')
 <div class="container mt-4">
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,4 +1,5 @@
-@extends('admin.layout')
+@extends('admin.layout.master')
+
 
 @section('content')
 <div class="container mt-4">
